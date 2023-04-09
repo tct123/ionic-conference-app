89afb50835a8a3ae58dcdd9439c42da8cd4ae61c
tct123 <42028373+tct123@users.noreply.github.com>
2023-04-09 14:49:18 +0200

    Update ExampleUnitTest.java

--- android/app/src/test/java/com/getcapacitor/myapp/ExampleUnitTest.java
+++ android/app/src/test/java/com/getcapacitor/myapp/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package com.getcapacitor.myapp;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
